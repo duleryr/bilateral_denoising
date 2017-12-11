1d8eb7668efff6cd4cc5caca329fab9869465c54
Romain Duléry <dulery.r@gmail.com>
2017-12-11 16:40:21 +0100

    génération fichiers sortie

--- generate_results.sh
+++ generate_results.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-for nb_iter in 1 3 5
+for nb_iter in 1 2 3 4 5
 do
-    for neighboorhoodSize in 1 3
+    for neighboorhoodSize in 1 2 3
     do
         echo "file : bone_gaussien.off, nb_iter = $nb_iter, neighboorhoodSize = $neighboorhoodSize"
         ./main "OFF_Files_Noised/bone_gaussien.off" "$nb_iter" "$neighboorhoodSize"
